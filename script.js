cf9ce61c33a65c58ae380ba15077cdcdaae04b12
aswanth-k-p <aswanthkp99@gmail.com>
2024-10-14 18:33:41 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let buttons = document.querySelectorAll(".button");
+let reset = document.querySelector(".reset");
+let popup = document.getElementById("popup");
+let winnerMessage = document.getElementById("winner-message");
+let newGameButton = document.getElementById("new-game-button");
+let turnO = true;
+
+const win = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+];
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.innerText === "") {
+            button.innerText = turnO ? "O" : "X";
+            button.disabled = true;
+            turnO = !turnO;
+            wincheck();
+        }
+    });
+});
+
+const wincheck = () => {
+    for (let pattern of win) {
+        let position1val = buttons[pattern[0]].innerText;
+        let position2val = buttons[pattern[1]].innerText;
+        let position3val = buttons[pattern[2]].innerText;
+
+        if (position1val && position1val === position2val && position1val === position3val) {
+            displayWinner(position1val);
+            disableButtons();
+            return;
+        }
+    }
+};
+
+const displayWinner = (winner) => {
+    winnerMessage.innerText = `Winner is ${winner}`;
+    popup.style.visibility="visible"
+};
+
+const disableButtons = () => {
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+};
+
+const enableButtons = () => {
+    buttons.forEach(button => {
+        button.disabled = false;
+        button.innerText = "";
+    });
+};
+
+const resetGame = () => {
+    turnO = true;
+    enableButtons();
+    
+};
+
+reset.addEventListener("click", resetGame);
+
+newGameButton.addEventListener("click", ()=>{
+    resetGame();
+    popup.style.visibility="hidden";
+});
